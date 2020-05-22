e60365365e0c193c9204acf87ec166116bb05c26
Saumel <saumelcaro@outlook.com>
2020-05-21 21:54:48 -0400

    change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World, Im the king');
 });
 
 app.listen(PORT, HOST);
